46e66264c7878dddcabdb76e2ea742fcc646fd80
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-12-02 22:06:35 +0000

    Button needs pullup; classifying event type

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -10,17 +10,22 @@ class PINS(IntEnum):
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PINS.CLK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PINS.DT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(PINS.BTN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PINS.BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 counter = 0
 clkLastState = GPIO.input(PINS.CLK)
 
-def my_callback(channel):
-    print(f"Callback on {channel}")
+def both_callback(channel):
+    btn = PINS(channel)
+    print("Event for " + repr(btn) + ". Type: ", end="")
+    if GPIO.input(channel):
+        print(f"rising")
+    else:
+        print(f"falling")
 
-GPIO.add_event_detect(PINS.CLK, GPIO.BOTH, callback=my_callback, bouncetime=20)
-GPIO.add_event_detect(PINS.DT, GPIO.BOTH, callback=my_callback, bouncetime=20)
-GPIO.add_event_detect(PINS.BTN, GPIO.BOTH, callback=my_callback, bouncetime=20)
+GPIO.add_event_detect(PINS.CLK, GPIO.BOTH, callback=both_callback, bouncetime=20)
+GPIO.add_event_detect(PINS.DT, GPIO.BOTH, callback=both_callback, bouncetime=20)
+GPIO.add_event_detect(PINS.BTN, GPIO.BOTH, callback=both_callback, bouncetime=20)
 
 try:
     while True:
