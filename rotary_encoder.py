20e24703dc08b96d268850ee8d47fbd4c713a63f
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-12-06 21:21:43 +0000

    Making it interrupt based instead
    - Fixes glitching when rotating clockwise

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -8,36 +8,44 @@ class PINS(IntEnum):
     BTN = 13
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PINS.CLK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(PINS.DT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PINS.CLK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PINS.DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PINS.BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 counter = 0
 clkLastState = GPIO.input(PINS.CLK)
 
+clkState = clkLastState = GPIO.input(PINS.CLK)
+dtState = GPIO.input(PINS.DT)
 def both_callback(channel):
+    global clkState, clkLastState, dtState, counter
     btn = PINS(channel)
-    print("Event for " + repr(btn) + ". Type: ", end="")
-    if GPIO.input(channel):
-        print(f"rising")
+    value = GPIO.input(channel)
+    if btn == PINS.CLK:
+        clkState = value
+    elif btn == PINS.DT:
+        dtState = value
     else:
-        print(f"falling")
+        print("Event for " + repr(btn) + f" with value={value}. Type: ", end="")
 
-GPIO.add_event_detect(PINS.CLK, GPIO.BOTH, callback=both_callback, bouncetime=20)
-GPIO.add_event_detect(PINS.DT, GPIO.BOTH, callback=both_callback, bouncetime=20)
-GPIO.add_event_detect(PINS.BTN, GPIO.BOTH, callback=both_callback, bouncetime=20)
+    clkState = GPIO.input(PINS.CLK)
+    dtState = GPIO.input(PINS.DT)
+    if clkState != clkLastState:
+        if dtState != clkState:
+            counter += 1
+        else:
+            counter -= 1
+        print(f"Counter: {counter}")
+    clkLastState = clkState
+
+GPIO.add_event_detect(PINS.CLK, GPIO.BOTH, callback=both_callback)
+GPIO.add_event_detect(PINS.DT,  GPIO.BOTH, callback=both_callback)
+GPIO.add_event_detect(PINS.BTN, GPIO.BOTH, callback=both_callback)
 
 try:
     while True:
-        clkState = GPIO.input(PINS.CLK)
-        dtState = GPIO.input(PINS.DT)
-        if clkState != clkLastState:
-            if dtState != clkState:
-                counter += 1
-            else:
-                counter -= 1
-            print(f"Counter: {counter}")
-        clkLastState = clkState
         sleep(0.01)
+except KeyboardInterrupt:
+    pass
 finally:
     GPIO.cleanup()
