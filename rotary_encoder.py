5ecd7a5e83b5fee496f92586c263f2dc05653973
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-12-06 21:42:11 +0000

    Refactoring: Rotary encoder as a class

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -1,6 +1,11 @@
 from RPi import GPIO
 from time import sleep
 from enum import IntEnum
+from dataclasses import dataclass, field
+from logging import getLogger, basicConfig, INFO
+
+logger = getLogger(__name__)
+basicConfig(format='%(levelname)s:%(message)s', level=INFO)
 
 class PINS(IntEnum):
     CLK = 5
@@ -12,35 +17,48 @@ GPIO.setup(PINS.CLK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PINS.DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PINS.BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-counter = 0
-clkLastState = GPIO.input(PINS.CLK)
 
-clkState = clkLastState = GPIO.input(PINS.CLK)
-dtState = GPIO.input(PINS.DT)
-def both_callback(channel):
-    global clkState, clkLastState, dtState, counter
-    btn = PINS(channel)
-    value = GPIO.input(channel)
-    if btn == PINS.CLK:
-        clkState = value
-    elif btn == PINS.DT:
-        dtState = value
-    else:
-        print("Event for " + repr(btn) + f" with value={value}. Type: ", end="")
+@dataclass
+class RotaryEncoder:
+    counter : int = 0
 
-    clkState = GPIO.input(PINS.CLK)
-    dtState = GPIO.input(PINS.DT)
-    if clkState != clkLastState:
-        if dtState != clkState:
-            counter += 1
+    def __post_init__(self):
+        self.last_clk_state = self.clk_state = GPIO.input(PINS.CLK)
+        self.dt_state = GPIO.input(PINS.DT)
+
+        GPIO.add_event_detect(PINS.CLK, GPIO.BOTH, callback=self.rotation_callback, bouncetime=5)
+        GPIO.add_event_detect(PINS.BTN, GPIO.BOTH, callback=self.button_callback, bouncetime=5)
+
+    def rotation_callback(self, channel: int):
+        pin = PINS(channel)
+        value = GPIO.input(pin)
+
+        if value:
+            logger.debug(f"{pin} - rising")
         else:
-            counter -= 1
-        print(f"Counter: {counter}")
-    clkLastState = clkState
+            logger.debug(f"{pin} - falling")
 
-GPIO.add_event_detect(PINS.CLK, GPIO.BOTH, callback=both_callback)
-GPIO.add_event_detect(PINS.DT,  GPIO.BOTH, callback=both_callback)
-GPIO.add_event_detect(PINS.BTN, GPIO.BOTH, callback=both_callback)
+        self.clk_state = GPIO.input(PINS.CLK)
+        self.dt_state = GPIO.input(PINS.DT)
+        if self.clk_state != self.last_clk_state:
+            if self.dt_state != self.clk_state:
+                self.counter += 1
+            else:
+                self.counter -= 1
+            logger.info(f"Counter: {self.counter}")
+        self.last_clk_state = self.clk_state
+
+    def button_callback(self, channel: int):
+        pin = PINS(channel)
+        value = GPIO.input(pin)
+
+        if value:
+            logger.debug(f"{pin} - rising")
+        else:
+            logger.debug(f"{pin} - falling")
+
+
+rot_enc = RotaryEncoder()
 
 try:
     while True:
