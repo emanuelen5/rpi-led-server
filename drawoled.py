c781f3db854593b9ce13ea27a86e385e600e5c7b
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-02-21 12:11:49 +0100

    Skeleton methods for controlling OLED display

--- drawoled.py
+++ drawoled.py
@@ -1,50 +1,53 @@
 import oled
+import ssd1331
+import numpy as np
 from datetime import datetime
+from dataclasses import dataclass, field
 
-if __name__=="__main__":
-    oled.init()
-    oled.clear()
-    while True:
-        oled.string(0, 16, datetime.now().strftime("%Y-%m-%d %H:%M"), color=(255, 0, 0))
-        oled.display()
 
-# int main(int argc, char **argv)
-# {
-#     if(!bcm2835_init())
-#     {
-#         return -1;
-#     }
-#
-#     SSD1331_begin();
-#     SSD1331_clear();
-#     printf("OLED example. Press Ctrl + C to exit.\n");
-#     while(1)
-#     {
-#         struct timeval tv;
-#         struct tm *timenow;
-#
-#         gettimeofday(&tv, NULL);
-#         timenow = localtime(&tv.tv_sec);
-#
-#         char str_buf[50];
-#         sprintf(str_buf, "%02d", timenow->tm_hour);
-#         SSD1331_string(0, 16, str_buf, 12, 1, RED);
-#         SSD1331_char(26, 16, ':' , 12, 1,WHITE);
-#         sprintf(str_buf, "%02d", timenow->tm_min);
-#         SSD1331_string(38, 16, str_buf, 12, 1, GREEN);
-#         SSD1331_char(62, 16, ':' , 12, 1,WHITE);
-#         sprintf(str_buf, "%02d", timenow->tm_sec);
-#         SSD1331_string(74, 16, str_buf, 12, 1, BLUE);
-#         SSD1331_char(14, 32, '.' , 12, 1,WHITE);
-#         sprintf(str_buf, "%06ld", tv.tv_usec);
-#         SSD1331_string(26, 32, str_buf, 12, 1, PURPLE);
-#
-#         SSD1331_string(6, 46, "emaus demo", 12, 1, CYAN);
-#
-#         SSD1331_display();
-#
-#     }
-#     bcm2835_spi_end();
-#     bcm2835_close();
-#     return 0;
-# }
+@dataclass
+class Display:
+    buffer: np.ndarray = field(default_factory=lambda: np.zeros((64, 32, 3), dtype=np.float32))
+
+    def clear(self):
+        pass
+
+    def string(self, x, y, s: str, color=(1., 1., 1.)):
+        pass
+
+    def display(self):
+        pass
+
+
+class OLED:
+    def __enter__(self) -> Display:
+        self.open()
+        d = Display()
+        return d
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @staticmethod
+    def open():
+        if not ssd1331.init():
+            raise OSError("Could not initialize the BCM module")
+        ssd1331.begin()
+        ssd1331.clear()
+
+    @staticmethod
+    def close():
+        ssd1331.spi_end()
+        ssd1331.close()
+
+
+def main():
+    with OLED as o:
+        o.clear()
+        o.string(0, 16, datetime.now().strftime("%Y-%m-%d %H:%M"), color=(1., 0., 0.))
+        o.string(6, 46, "Emaus demo", color=(1., 1., 0.))
+        o.display()
+
+
+if __name__ == "__main__":
+    main()
