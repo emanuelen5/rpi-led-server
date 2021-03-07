be6994f712a51ee6e4fd0d07ac925de29ccec0dc
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-07 17:11:38 +0100

    Nicening up appearance the OpenCV variant of the display
    - Scaling up with nearest neighbor
    - Adding borders between pixels
    - Contrast adjustment so the color actually goes between min and max

--- oled/display/display.py
+++ oled/display/display.py
@@ -33,10 +33,21 @@ class Display:
 class OpenCVDisplay(Display):
     displayed: bool = False
 
-    def display(self):
+    def display(self, scaling=6.0):
         super().display()
-        buffer = liboled.get_buffer()
-        cv2.imshow("OLED_Display", cv2.resize(buffer, fx=3., fy=3., dsize=None, interpolation=None))
+        buffer = liboled.get_buffer().astype(np.float32)
+        buffer[:, :, 0] = buffer[:, :, 0] * 1.0 / 0xF8
+        buffer[:, :, 1] = buffer[:, :, 1] * 1.0 / 0xFC
+        buffer[:, :, 2] = buffer[:, :, 2] * 1.0 / 0xF8
+        buffer = cv2.resize(buffer, fx=scaling, fy=scaling, dsize=None, interpolation=cv2.INTER_NEAREST)
+        # Make grid
+        y, x, z = buffer.shape
+        X, Y, Z = np.meshgrid(range(x), range(y), range(z))
+        grid = (Y % scaling == scaling - 1) | (X % scaling == scaling - 1)
+        buffer[grid] = 0.1 + buffer[grid] * 0.9
+        # Make border
+        buffer = cv2.copyMakeBorder(buffer, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=(1., 1., 1.))
+        cv2.imshow("OLED_Display", buffer)
         if not self.displayed:
             self.displayed = True
             print("Press q to exit")
