7e312ac61e379097ec5d04585f0256dc3bd215aa
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-08 20:14:46 +0100

    Precalculating grid calculation

--- oled/display/display.py
+++ oled/display/display.py
@@ -32,19 +32,24 @@ class Display:
 @dataclass
 class OpenCVDisplay(Display):
     displayed: bool = False
+    scaling: float = 6.0
 
-    def display(self, scaling=6.0):
+    def __post_init__(self):
+        # Pre-compute grid
+        scaled_buffer = cv2.resize(self.buffer, fx=self.scaling, fy=self.scaling, dsize=None)
+        y, x, z = scaled_buffer.shape
+        X, Y, _ = np.meshgrid(range(x), range(y), range(z))
+        self.grid = (Y % self.scaling == self.scaling - 1) | (X % self.scaling == self.scaling - 1)
+
+    def display(self):
         super().display()
         buffer = liboled.get_buffer().astype(np.float32)
         buffer[:, :, 0] = buffer[:, :, 0] * 1.0 / 0xF8
         buffer[:, :, 1] = buffer[:, :, 1] * 1.0 / 0xFC
         buffer[:, :, 2] = buffer[:, :, 2] * 1.0 / 0xF8
-        buffer = cv2.resize(buffer, fx=scaling, fy=scaling, dsize=None, interpolation=cv2.INTER_NEAREST)
+        buffer = cv2.resize(buffer, fx=self.scaling, fy=self.scaling, dsize=None, interpolation=cv2.INTER_NEAREST)
         # Make grid
-        y, x, z = buffer.shape
-        X, Y, Z = np.meshgrid(range(x), range(y), range(z))
-        grid = (Y % scaling == scaling - 1) | (X % scaling == scaling - 1)
-        buffer[grid] = 0.1 + buffer[grid] * 0.9
+        buffer[self.grid] = 0.1 + buffer[self.grid] * 0.9
         # Make border
         buffer = cv2.copyMakeBorder(buffer, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=(1., 1., 1.))
         cv2.imshow("OLED_Display", buffer)
