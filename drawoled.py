3036a49a778dc94d1aba46e6cbb770d635b90b77
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-09 21:46:26 +0100

    Adding alpha to paste function

--- drawoled.py
+++ drawoled.py
@@ -8,9 +8,9 @@ def main():
         while True:
             display.clear()
             dt = datetime.now()
-            put_string(display.buffer, 0, 16, dt.strftime("%Y-%m-%d"), fg=(1., 0., 0.))
-            put_string(display.buffer, 0, 26, dt.strftime("%H:%M:%S.%f"), fg=(1., 0., 1.))
-            put_string(display.buffer, 0, 46, "Emaus demo", fg=(1., 1., 0.))
+            put_string(display.buffer, 0, 26, dt.strftime("%H:%M:%S.%f"), fg=(1., 0., 1.), bg=(1., 1., 1.), alpha=0.3)
+            put_string(display.buffer, 0, 16, dt.strftime("%Y-%m-%d"), fg=(1., 0., 0.), bg=None, alpha=1)
+            put_string(display.buffer, 0, 36, "Emaus demo", bg=(1., 1., 0.), fg=None, alpha=0.7)
             display.display()
 
 
