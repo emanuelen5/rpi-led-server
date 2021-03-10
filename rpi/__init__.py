ec46bf034d381d6250ac2713a19df4b372b589c6
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-10 22:41:21 +0100

    Try/catch import instead of guessing platform

--- rpi/__init__.py
+++ rpi/__init__.py
@@ -1,14 +1,6 @@
-from util import is_raspberry_pi
-
-if not is_raspberry_pi():
-    # Replace libraries by fake ones
-    import sys
-    import fake_rpi
-
-    sys.modules['RPi'] = fake_rpi.RPi  # Fake RPi
-    sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO  # Fake GPIO
-    sys.modules['smbus'] = fake_rpi.smbus  # Fake smbus (I2C)
-
-from RPi import GPIO
+try:
+    from RPi import GPIO
+except ImportError:
+    from fake_rpi.RPi import GPIO
 
 __all__ = ["GPIO"]
