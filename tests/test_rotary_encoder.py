f7619384f01b8c9d0dad56aded6f8eed4629e867
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-11 20:37:49 +0100

    Creating two base classes for mocking the GPIO and rotary encoder

--- tests/test_rotary_encoder.py
+++ tests/test_rotary_encoder.py
@@ -3,30 +3,43 @@ from rotary_encoder import RotaryEncoder, PINS, GPIO
 from unittest.mock import MagicMock, patch
 
 
-class TestSmoke(unittest.TestCase):
+class BaseGPIO_Test(unittest.TestCase):
     def setUp(self) -> None:
-        self.rotenc = RotaryEncoder()
-        self.pin_values = {PINS.CLK: False, PINS.DT: False, PINS.BTN: False}
-
-    def tearDown(self) -> None:
-        GPIO.cleanup()
-
-    @patch("rotary_encoder.GPIO.input")
-    def test_calls_rotate_callback_on_clockwise(self, input_mock: MagicMock):
-        cb = MagicMock()
+        patcher = patch("rpi.GPIO.input")
+        self.input_mock = patcher.start()
+        self.addCleanup(patcher.stop)
+        self._pin_values = {PINS.CLK: False, PINS.DT: False, PINS.BTN: False}
 
         def get_pin_value(pin):
-            return self.pin_values[pin]
+            if pin not in self._pin_values:
+                raise ValueError(f"The pin {pin} has not been set before the test")
+            return self._pin_values[pin]
 
-        input_mock.side_effect = get_pin_value
-        self.rotenc.register_rotation_callback(cb)
+        self.input_mock.side_effect = get_pin_value
+
+    def set_pin(self, pin: PINS, val: bool):
+        if pin not in self._pin_values:
+            raise ValueError(f"The pin {pin} is not controlled by the mock")
+        self._pin_values[pin] = val
+
+
+class BaseTestRotaryEncoder(BaseGPIO_Test):
+    def setUp(self) -> None:
+        super().setUp()
+        self.rotenc = RotaryEncoder()
+        self.addCleanup(GPIO.cleanup)
 
         # Make sure we are at a known state first
-        self.pin_values[PINS.CLK] = False
+        self.set_pin(PINS.CLK, False)
         self.rotenc.rotation_callback(PINS.CLK)
 
-        # Now run test
-        self.pin_values[PINS.CLK] = True
+
+class TestSmoke(BaseTestRotaryEncoder):
+    def test_calls_rotate_callback_on_clockwise(self):
+        cb = MagicMock()
+        self.rotenc.register_rotation_callback(cb)
+
+        self.set_pin(PINS.CLK, True)
         self.rotenc.rotation_callback(PINS.CLK)
         cb.assert_called_with(True)
 
