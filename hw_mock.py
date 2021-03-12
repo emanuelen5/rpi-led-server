4882606e0f087f76f541217bda2008ffdc5c6d4e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-12 23:49:52 +0100

    Using arrows for control; implementing press

--- hw_mock.py
+++ hw_mock.py
@@ -1,8 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from unittest.mock import patch, MagicMock
 import cv2
 import numpy as np
 import math
+import time
 
 
 @dataclass
@@ -11,6 +12,9 @@ class RotatyEncoderHW_Model:
     PIN_BTN: int
     PIN_DT: int
     steps: int = 15
+    pressed: bool = False
+    pressed_time: float = field(repr=False, init=False, default=0)
+    timeout: float = 0.1
 
     def __post_init__(self):
         self._clk_event = []
@@ -31,32 +35,43 @@ class RotatyEncoderHW_Model:
         elif pin == self.PIN_DT:
             self._dt_event.append(callback)
 
+    def press(self):
+        self.pressed = True
+        self.pressed_time = time.time()
+
     def rotate(self, clockwise: bool):
         self.rotation += 1 if clockwise else -1
         for cb in self._clk_event:
             cb(self.PIN_CLK)
 
     def refresh(self):
-        disp = np.zeros((200, 200, 3), dtype=np.float32)
+        disp = np.zeros((200, 200, 3), dtype=np.uint8)
+        radius = 40 if self.pressed else 50
+        if self.pressed and time.time() - self.pressed_time > self.timeout:
+            self.pressed = False
         rotation = self.rotation * self.rotation_per_step
-        pt2 = np.array([100, 100]) + np.array([math.sin(rotation), -math.cos(rotation)]) * 50
+        pt2 = np.array([100, 100]) + np.array([math.sin(rotation), -math.cos(rotation)]) * radius
         cv2.line(disp, (100, 100), tuple(pt2.astype(np.int16)), (123, 50, 168), thickness=3, lineType=cv2.LINE_AA)
-        cv2.circle(disp, (100, 100), 50, (255, 255, 255), thickness=5, lineType=cv2.LINE_AA)
+        cv2.circle(disp, (100, 100), radius, (255, 255, 255), thickness=5, lineType=cv2.LINE_AA)
         cv2.imshow("rotary_encoder_hw", disp)
+
+
+RIGHT_ARROW = 65363
+LEFT_ARROW = 65361
+UP_ARROW = 65362
+DOWN_ARROW = 65364
 
 
 def main():
     rot = RotatyEncoderHW_Model(1, 2, 3)
-    rot.refresh()
     while (k := cv2.waitKeyEx(1)) != ord("q"):
-        if k == ord("h"):
+        if k == LEFT_ARROW:
             rot.rotate(False)
-            rot.refresh()
-        elif k == ord("l"):
+        elif k == RIGHT_ARROW:
             rot.rotate(True)
-            rot.refresh()
-        elif k != -1:
-            print(k)
+        elif k == DOWN_ARROW:
+            rot.press()
+        rot.refresh()
 
 
 if __name__ == "__main__":
