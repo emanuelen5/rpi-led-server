194f1c42ed33b1b73b0d82e5df78d76eaa5dc249
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-04-03 19:32:12 +0200

    Testing caching function

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,11 +1,54 @@
 import unittest
 import util
+from unittest.mock import MagicMock
+import time
+
+
+class TestCache(unittest.TestCase):
+    def test_cache_long(self):
+        m = MagicMock()
+        patched_function = util.max_update_rate(1e9)(m)
+
+        m.assert_not_called()
+        # Call the function twice, and check if it caches
+        patched_function()
+        patched_function()
+        self.assertEqual(1, m.call_count)
+
+    def test_cache_medium(self):
+        m = MagicMock()
+        timeout = 0.01
+        patched_function = util.max_update_rate(timeout)(m)
+
+        m.assert_not_called()
+        # Call the function twice, and check if it caches
+        patched_function()
+        patched_function()
+        self.assertEqual(1, m.call_count)
+
+        time.sleep(timeout + 0.001)
+        patched_function()
+        self.assertEqual(2, m.call_count)
+        patched_function()
+        self.assertEqual(2, m.call_count)
+
+    def test_cache_short(self):
+        m = MagicMock()
+        patched_function = util.max_update_rate(0)(m)
+
+        # Call the function twice, and check if it caches
+        patched_function()
+        patched_function()
+        self.assertEqual(2, m.call_count)
+
 
 
 class TestIP(unittest.TestCase):
     def test_ip_is_sane(self):
-        ip = util.get_ip()
-        self.assertRegex(ip, r'^\d+\.\d+\.\d+\.\d+$')
+        ips = util.get_ips()
+        self.assertIsInstance(ips, tuple)
+        for ip in ips:
+            self.assertRegex(ip, r'^\d+\.\d+\.\d+\.\d+$')
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ from enum import IntEnum
 import os
 import socket
 import subprocess
+import time
 
 
 class KeyCode(IntEnum):
@@ -14,9 +15,38 @@ class KeyCode(IntEnum):
         return self.name
 
 
-def get_ip():
+def run_command(cmd):
+    p = subprocess.run(cmd, capture_output=True, shell=True, encoding="utf-8")
+    return p.stdout.strip()
+
+
+def max_update_rate(update_rate: float = 1.0):
+    def wrapper(f):
+        f.last_update_time = 0
+
+        def cache_wrapper(*args, **kwargs):
+            now = time.time()
+            if now - f.last_update_time > update_rate:
+                f.cached_value = f(*args, **kwargs)
+                f.last_update_time = now
+            return f.cached_value
+
+        return cache_wrapper
+
+    return wrapper
+
+
+@max_update_rate(1.0)
+def get_ips():
     if os.name == "nt":
-        return socket.gethostbyname(socket.gethostname())
+        return tuple(socket.gethostbyname(socket.gethostname()))
     else:
-        p = subprocess.run("hostname -I", capture_output=True, shell=True, encoding="utf-8")
-        return p.stdout.strip()
+        return tuple(run_command("hostname -I").split(' '))
+
+
+@max_update_rate(3.0)
+def get_uptime():
+    if os.name == "nt":
+        return "Unknown"
+    else:
+        return run_command("uptime")
