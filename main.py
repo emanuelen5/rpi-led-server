7a9e382cff177dac4d7f4cf20eeb860356546575
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-04-05 14:47:16 +0200

    Making header and notification scrolling lines

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from oled import OLED
 from oled.display.display import DisplayModelViewer
 from oled.fonts import put_string
+from oled.scrolling_text import ScrollingLine
 from datetime import datetime
 from enum import Enum, auto
 from leds import create_pixels
@@ -83,7 +84,9 @@ class Globals:
     buffer_leds: np.ndarray = np.empty((1, 1, 1))
     buffer_rotenc: np.ndarray = np.empty((1, 1, 1))
     keypress_rotenc = queue.Queue()
-    notifications = ["Homeassistant"]
+    header_line = ScrollingLine("SEL: ")
+    value_line = ScrollingLine("=")
+    notifications = [ScrollingLine("1: Homeassistant")]
 
 
 def main_display():
@@ -94,7 +97,8 @@ def main_display():
             if time.time() - Globals.last_interaction > Globals.screen_saver_time:
                 time.sleep(0.2)
                 continue
-            put_string(display.buffer, 0, 0,  f"SEL:{Globals.select_mode.name}", fg=(1., 1., 1.), bg=None)
+            Globals.header_line.string = f"SEL: {Globals.select_mode.name}"
+            Globals.header_line.render(display.buffer, 0, fg=(1., 1., 1.), bg=None)
             if Globals.select_mode == SelectMode.LED_EFFECT:
                 put_string(display.buffer, 0, 12, f"={Globals.led_mode.name}", fg=(1., 1., 1.), bg=None)
             elif Globals.select_mode == SelectMode.EFFECT_SPEED:
@@ -119,8 +123,7 @@ def main_display():
                     put_string(display.buffer, 0, 26, "No notifications", fg=(0., 1., 0.))
                 else:
                     for i, notification in enumerate(Globals.notifications):
-                        put_string(display.buffer, 0, 26 + i * 12, f"{i+1}:", bg=None)
-                        put_string(display.buffer, 12, 26 + i * 12, str(notification), bg=None)
+                        notification.render(display.buffer, 26 + i * 12, bg=None)
             elif Globals.main_mode == MainMode.STATUS:
                 put_string(display.buffer, 0, 26, f"IP:{', '.join(util.get_ips())}")
                 put_string(display.buffer, 0, 38, util.get_uptime())
