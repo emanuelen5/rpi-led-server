10c6daeed1b06c34b80d58303d9f47390eefc292
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-04-06 20:49:30 +0200

    Property as private; converting to abstract methods

--- leds/model.py
+++ leds/model.py
@@ -1,6 +1,6 @@
 from typing import Union, Tuple, List
 import numpy as np
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 
 
@@ -11,9 +11,20 @@ ND_LIKE = Union[Tuple[int, int, int], List[int], np.ndarray]
 class LED_BaseModel(ABC):
     buffer: np.ndarray = field(init=False, repr=False)
 
+    @abstractmethod
     def __len__(self): ...
+
+    @abstractmethod
     def __getitem__(self, item: int): ...
+
+    @abstractmethod
     def __setitem__(self, key: int, value: ND_LIKE): ...
+
+    @abstractmethod
     def __iter__(self): ...
+
+    @abstractmethod
     def fill(self, value: ND_LIKE): ...
+
+    @abstractmethod
     def show(self): ...
