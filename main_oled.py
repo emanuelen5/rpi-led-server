1b867543001cd20498a5da87cc06c93d8c663a96
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-04-27 22:05:21 +0200

    Fix: Buffer was renamed

--- main_oled.py
+++ main_oled.py
@@ -39,14 +39,14 @@ def main():
             display.clear()
             idx = (time.time() - start_time) // 10 % 3
             if idx == 0:
-                display.buffer = image_smol1.copy()
+                display.back_buffer = image_smol1.copy()
             elif idx == 1:
-                display.buffer = image_smol2.copy()
+                display.back_buffer = image_smol2.copy()
             elif idx == 2:
                 dt = datetime.now()
-                put_string(display.buffer, 0, 26, dt.strftime("%H:%M:%S.%f"), fg=(1., 0., 1.), bg=(1., 1., 1.), alpha=0.3)
-                put_string(display.buffer, 0, 16, dt.strftime("%Y-%m-%d"), fg=(0., 0., 1.), bg=None, alpha=1)
-                put_string(display.buffer, 0, 36, "Emaus demo", bg=(0., 1., 1.), fg=None, alpha=0.7)
+                put_string(display.back_buffer, 0, 26, dt.strftime("%H:%M:%S.%f"), fg=(1., 0., 1.), bg=(1., 1., 1.), alpha=0.3)
+                put_string(display.back_buffer, 0, 16, dt.strftime("%Y-%m-%d"), fg=(0., 0., 1.), bg=None, alpha=1)
+                put_string(display.back_buffer, 0, 36, "Emaus demo", bg=(0., 1., 1.), fg=None, alpha=0.7)
 
             display.refresh()
             if show_viewer:
